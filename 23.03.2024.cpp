5d64b8ec2738a1e35c0ba41c84cb7a6519cfcaa3
Masked-coder11 <153896221+Masked-coder11@users.noreply.github.com>
2024-03-23 00:32:48 +0530

    Create 23.03.2024.cpp

---
+++ 23.03.2024.cpp
@@ -0,0 +1,17 @@
+lass Solution {
+  public:
+    vector<int> Series(int n) {
+        // Code here
+        vector<int>ans(n+1);
+        ans[0]=0;
+        ans[1]=1;
+        
+        int mod=1e9+7;
+        for(int i=2;i<=n;i++){
+            int x= ans[i-1]+ans[i-2];
+            x%=mod;
+            ans[i]=x;
+        }
+        return ans;
+    }
+};
