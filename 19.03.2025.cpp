363b0c833d6bac6dd07b682cf69f15d99dd49738
Masked-coder11 <153896221+Masked-coder11@users.noreply.github.com>
2025-03-19 07:31:17 +0530

    Create 19.03.2025.cpp

---
+++ 19.03.2025.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int helper(int ind, int mode, int k, vector<int>&prices, vector<vector<vector<int>>>&dp){
+        if(ind==prices.size()) return 0;
+        if(k==0) return 0;
+        if(dp[ind][k][mode]!=-1) return dp[ind][k][mode];
+        
+        if(mode==0){
+            // buy
+            return dp[ind][k][mode] = max(helper(ind+1, !mode, k,prices, dp)-prices[ind],
+                        helper(ind+1, mode, k, prices, dp));
+        }
+        else{
+            return dp[ind][k][mode] = max(helper(ind+1, !mode, k-1, prices, dp)+prices[ind],
+            helper(ind+1, mode, k, prices, dp));
+        }
+    }
+  
+    int maxProfit(vector<int>& prices, int k) {
+        // code here
+        int n=prices.size();
+        
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(k+1, vector<int>(2,-1)));
+        
+        return helper(0, 0, k, prices, dp);
+        
+    }
+};
